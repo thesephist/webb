d603f069d7e4daf7d281e4d9e61083ff01b2735f
Linus Lee <100479lee@gmail.com>
2015-11-20 02:16:55 -0500

    Organized code formatting

--- to_graph.js
+++ to_graph.js
@@ -14,6 +14,7 @@ var result = "Matrix[]";
 result = result.substring(8,result.length - 2); // so in the form "1.0, 1.0, 1.0], [1.0, 1.0, 1.0" and so on
 
 var resultArray = result.split("], ["); // so each element in the form 1.0, 1.0, 1.0 and so on
+
 resultArray.forEach(function(string){
     links.push(string.split(", "));
 });
@@ -23,14 +24,18 @@ pop_size = resultArray.length;
 
 // have node coordinates put in array of duples
 var angle = Math.PI / pop_size * 2;
-for(i=0;i<pop_size;i++){
-    node_coords.push([500 - 500 * Math.sin(angle * i).toString(), 500 - 500 * Math.cos(angle * i).toString()]);
+for(i=0; i<pop_size; i++){
+    node_coords.push(
+        [500 - 500 * Math.sin(angle * i).toString(), 
+         500 - 500 * Math.cos(angle * i).toString()]
+    );
 }
 
-// have segment startNode, endNode, strength in array, will resolve node coordinates from node list
+// have segment startNode, endNode, strength in array; will resolve node coordinates from node list
 var maximum=0.0;
 var minimum=1.0;
 var linkc = links;
+
 for(i=0; i<pop_size; i++){
     for(j=0; j<pop_size; j++){
         links[i][j] = parseFloat(links[i][j]);
@@ -43,16 +48,15 @@ for(i=0; i<pop_size; i++){
 }
 for(i=0; i<pop_size; i++){
     for(j=0; j<pop_size; j++){
-        if(i!=j){
+        if(i != j){
             if(links[i][j]>maximum){maximum = links[i][j]}
-            if(links[i][j]<minimum){minimum = links[i][j]}
-    
+            if(links[i][j]<minimum){minimum = links[i][j]}   
         }
     }
 }
 
 var node_sizes = [];
-for (i=0; i<pop_size; i++) {
+for (i = 0; i < pop_size; i++) {
     sum = 0;
     for (j=0; j<pop_size; j++) {
         sum += links[i][j];
@@ -69,10 +73,10 @@ SVGlines = ["<svg xmlns='http://www.w3.org/2000/svg' version='1.0' viewBox='0 0 
 // write lines first
 var writtenlines = ""
 SVGlines.push("<!-- connections -->");
-for(i=0;i<pop_size;i++){
-    for(j=0;j<pop_size;j++){
-        writtenlines+="0x"+node_coords[i]+"x0"+node_coords[j];
-        if(writtenlines.indexOf("0x"+node_coords[j]+"x0"+node_coords[i])==-1){
+for(i = 0; i < pop_size;i++){
+    for(j = 0; j < pop_size; j++){
+        writtenlines += "0x" + node_coords[i] + "x0" + node_coords[j];
+        if(writtenlines.indexOf("0x" + node_coords[j] + "x0" + node_coords[i]) == -1){
             SVGlines.push("<line x1='" + node_coords[i][0] + "' y1='" + node_coords[i][1] + "' x2='" + node_coords[j][0] + "' y2='" + node_coords[j][1] + "' style='stroke:#000;stroke-width:2;opacity:" + (((links[i][j] - minimum) / (maximum - minimum))).toString() + "'/>");
         }
     }
@@ -94,8 +98,7 @@ SVGlines.forEach(function(line){
     fs.appendFile("graph.svg", line, function(err) {
         if(err) {
             return console.log(err);
-        }
-        
+        }        
     });
 });
 
@@ -103,4 +106,4 @@ fs.appendFile("graph.svg", "</svg>", function(err) {
     if(err) {return console.log(err);}
 });
           
-console.log("Graph Saved!");
+console.log("Graph Generated");
